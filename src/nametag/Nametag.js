2d9924cd66618a01b2918dca1b301fd0eec84173
Dustin Gaudet <dustin@dustingaudet.com>
2017-03-13 23:52:29 -0400

    Add basic nametag style/text

--- src/nametag/Nametag.js
+++ src/nametag/Nametag.js
@@ -5,8 +5,13 @@ class Nametag extends Component {
   render() {
     return (
       <div className="nametag">
-        <h1>Nametag</h1>
-
+        <div className="heading">
+          <h1>Hello</h1>
+          <h2>my name is</h2>
+        </div>
+        <div className="name">
+          <h3>Jed the Robo</h3>
+        </div>
       </div>
     );
   }
