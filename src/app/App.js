5ee2b48f8022774c27e3f22a7db6d91edce70dd6
Dustin Gaudet <dustin@dustingaudet.com>
2017-03-13 23:37:26 -0400

    Add Nametag component skeleton.

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nametag from '../nametag/Nametag'
  
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Nametag />
       </div>
     );
   }
---
+++ src/nametag/Nametag.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Nametag.css';
+ 
+class Nametag extends Component {
+  render() {
+    return (
+      <div className="nametag">
+        <h1>Nametag</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Nametag;
